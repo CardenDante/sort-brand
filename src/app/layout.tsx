07808d15b65e474e38927a59db98040655636f06
CardenDante <cardendante@gmail.com>
2025-08-08 14:40:39 +0300

    ClarityScript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import TopBanner from '@/components/TopBanner';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import BackToTop from '@/components/BackToTop';
+import ClarityScript from '@/components/ClarityScript';
 
 export const metadata = {
   title: 'Sortbrands - Digital Creative Agency',
@@ -22,6 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+      <ClarityScript />
       </head>
       <body className="font-[Poppins]">
         <TopBanner />
